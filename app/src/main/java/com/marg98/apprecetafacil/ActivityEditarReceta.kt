f996343f4e218f83e4d38184f9c8b84aab4a4320
Maximo <guzmanmax1998@gmail.com>
2023-09-26 13:57:55 -0600

    Base

---
+++ app/src/main/java/com/marg98/apprecetafacil/ActivityEditarReceta.kt
@@ -0,0 +1,11 @@
+package com.marg98.apprecetafacil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityEditarReceta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
