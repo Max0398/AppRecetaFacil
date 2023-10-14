148f3349647fb74be392cdc19f211c1a5aae688e
Christopher <rioschristopher.ht@gmail.com>
2023-10-13 21:04:49 -0600

    Añadi la interfaces de login y menu, att Christopher Rios

---
+++ app/src/main/java/com/marg98/apprecetafacil/ActivityMenuReceta.kt
@@ -0,0 +1,11 @@
+package com.marg98.apprecetafacil
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class ActivityMenuReceta : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_receta)
+    }
+}
